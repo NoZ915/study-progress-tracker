387f9d525e49169b236c43cf7c060d405964438e
NoZ915 <tina0915tw0915@gmail.com>
2025-01-28 15:14:14 +0800

    feat:Add hover on materials cards

--- frontend/pages/MaterialsPage.jsx
+++ frontend/pages/MaterialsPage.jsx
@@ -1,5 +1,7 @@
 import { useGetAllMaterials } from "../hooks/materials/useGetAllMaterials.js";
 import { Container, Text, Grid, Card, Loader, Image } from "@mantine/core";
+import { Link } from "react-router-dom";
+import styles from "./MaterialsPage.module.css"; 
 
 function MaterialsPage() {
     const { data: materials, isLoading } = useGetAllMaterials();
@@ -14,26 +16,29 @@ function MaterialsPage() {
                 <Grid gutter="md">
                     {materials.map((material) => (
                         <Grid.Col key={material.material_id} span={4}>
-                            <Card
-                                shadow="sm"
-                                radius="md"
-                                withBorder
-                                style={{
-                                    backgroundColor: "white",
-                                    height: 400
-                                }}
-                            >
-                                <Card.Section style={{marginBottom: 10, height: 350, overflow: "hidden"}}>
-                                    <Image
-                                        src={material.image_url}
-                                        alt={material.title}
-                                        fix="cover"
-                                    />
-                                </Card.Section>
-                                <Text weight={500} size="lg" align="center">
-                                    {material.title}
-                                </Text>
-                            </Card>
+                            <Link style={{textDecoration: "none"}} to={`/material/${material.material_id}`}>
+                                <Card
+                                    shadow="sm"
+                                    radius="md"
+                                    withBorder
+                                    className={styles.card}
+                                    style={{
+                                        backgroundColor: "white",
+                                        height: 400
+                                    }}
+                                >
+                                    <Card.Section style={{ marginBottom: 10, height: 350, overflow: "hidden" }}>
+                                        <Image
+                                            src={material.image_url}
+                                            alt={material.title}
+                                            fix="cover"
+                                        />
+                                    </Card.Section>
+                                    <Text weight={500} size="lg" align="center">
+                                        {material.title}
+                                    </Text>
+                                </Card>
+                            </Link>
                         </Grid.Col>
                     ))}
                 </Grid>
