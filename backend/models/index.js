621687d6bdb03eeccbfbbeacddcd3c9af1541e0e
NoZ915 <tina0915tw0915@gmail.com>
2025-02-07 21:52:16 +0800

    fix:Typo

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,18 +1,18 @@
-import { Users } from "./Users.js";
+import { User } from "./Users.js";
 import { Material } from "./Material.js";
-import { Sessions } from "./Sessions.js";
+import { Session } from "./Sessions.js";
 import { UserMaterials } from "./UserMaterials.js";
 import { Progress } from "./Progress.js";
 
 // 設定關聯
-Users.hasMany(UserMaterials, { foreignKey: "user_id" });
+User.hasMany(UserMaterials, { foreignKey: "user_id" });
 Material.hasMany(UserMaterials, { foreignKey: "material_id" });
 
-UserMaterials.belongsTo(Users, { foreignKey: "user_id" });
+UserMaterials.belongsTo(User, { foreignKey: "user_id" });
 UserMaterials.belongsTo(Material, { foreignKey: "material_id" });
 UserMaterials.hasMany(Progress, { foreignKey: "user_material_id" });
 
 Progress.belongsTo(UserMaterials, { foreignKey: "user_material_id" });
-Progress.belongsTo(Sessions, { foreignKey: "session_id" });
+Progress.belongsTo(Session, { foreignKey: "session_id" });
 
-export { Users, Material, Sessions, UserMaterials, Progress };
+export { User, Material, Session, UserMaterials, Progress };
