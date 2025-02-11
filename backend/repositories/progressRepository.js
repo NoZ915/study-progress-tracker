9da4a95a67dee93327d997ab946cdf12264ff72c
NoZ915 <tina0915tw0915@gmail.com>
2025-02-11 23:40:29 +0800

    update:Let update progress has create function if progress not exist

--- backend/repositories/progressRepository.js
+++ backend/repositories/progressRepository.js
@@ -8,12 +8,17 @@ class ProgressRepository {
   }
 
   async updateProgress(user_material_id, session_id, completion_time, correction_time, notes, user_id) {
-    return await Progress.update(
-      { completion_time, correction_time, notes },
-      {
-        where: { user_material_id, session_id, user_id },
-      }
-    );
+    const existingProgress = await Progress.findOne({
+      where: { user_material_id, session_id, user_id }
+    });
+
+    if (existingProgress) {
+      // 更新
+      return await existingProgress.update({ completion_time, correction_time, notes });
+    } else {
+      // 創建
+      return await Progress.create({ user_material_id, session_id, completion_time, correction_time, notes, user_id });
+    }
   }
 }
 
