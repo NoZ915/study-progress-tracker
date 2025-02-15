5b9f98251b75bebf782d8e09a9291d35a9645e97
NoZ915 <tina0915tw0915@gmail.com>
2025-02-15 13:20:00 +0800

    feat:Add CreateNewUserMaterial func on btn

--- frontend/pages/MaterialDetailPage.jsx
+++ frontend/pages/MaterialDetailPage.jsx
@@ -1,10 +1,32 @@
 import { useParams } from "react-router-dom";
 import { Container, Text, Image, Button, Loader, Rating, Card, Center, Group } from "@mantine/core";
 import { useGetMaterialDetail } from "../hooks/materials/useGetMaterialDetail";
+import { useState } from "react";
+import ConfirmCancelModal from "../components/ConfirmCancelModal";
+import { useCreateNewUserMaterial } from "../hooks/userMaterials/useCreateNewUserMaterial";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 function MaterialDetailPage() {
+  const user = useAuthContext();
   const { material_id } = useParams();
   const { data: material, isLoading, isFetching } = useGetMaterialDetail(material_id);
+  const { mutate } = useCreateNewUserMaterial();
+
+  const [openCancelModal, setOpenCancelModal] = useState(false);
+  const handleCloseModal = () => {
+    setOpenCancelModal(false);
+  };
+  const handleConfirmCreateUserMaterial = () => {
+    if (!material_id) return;
+    mutate(
+      { user_id: user.user_id, material_id },
+      {
+        onSuccess: () => {
+          setOpenCancelModal(false);
+        },
+      }
+    )
+  };
 
   if (isLoading || isFetching) return <Loader size="lg" />;
 
@@ -30,9 +52,20 @@ function MaterialDetailPage() {
       </Group>
 
       <Group justify="center" mt="md">
-        <Button color="orange" variant="filled">加入進度</Button>
+        <Button color="orange" variant="filled" onClick={() => setOpenCancelModal(true)}>加入進度</Button>
         <Button color="lime.4" variant="filled" autoContrast>我要評價</Button>
       </Group>
+
+      <ConfirmCancelModal
+        opened={openCancelModal}
+        onClose={handleCloseModal}
+        onConfirm={handleConfirmCreateUserMaterial}
+        title="將要把該本講義加入您的進度"
+        message="確定加入進度嗎？"
+        confirmText="確定"
+        cancelText="取消"
+        confirmColor="blue"
+      />
 
       {material.feedbacks?.length > 0 ? (
         material.feedbacks.map((feedback) => (
